33f5da4c3ffc9f5b60458ad2c24763adf53dc5b1
420190038 <420190038@LRNWNWRK-011.learn.cpp.ac.nz>
2019-09-03 14:36:40 +1200

    Super Final Build with Comments

--- src/ticketingsystem/TicketingSystem.java
+++ src/ticketingsystem/TicketingSystem.java
@@ -11,16 +11,26 @@ import java.util.Queue;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
+/**
+ * TicketingSystem, runs method on time schedule demonstrates queue usage
+ * @author Matthew Dods 420190038
+ */
 public class TicketingSystem {
     public static Queue<Integer> queue = new LinkedList<>();
-    
+     
+    /**
+      * 
+      * @param args 
+      */
     public static void main(String[] args) {
         Timer timer = new Timer();
-        timer.schedule(new SalesAssisstantTask(), 0, 5000);
-        timer.schedule(new CustomerAdd(), 0, 3000);
+        timer.schedule(new SalesAssisstantTask(), 0, 5000); // every 5 seconds do this method.
+        timer.schedule(new CustomerAdd(), 0, 3000); // every 3 seconds do this method.
     }
     
+    /**
+     * SalesAssistantTask, Says what customer is seeing and removes top from queue (first in first out)
+     */
     public static class SalesAssisstantTask extends TimerTask { //inner class
        
         public void run() {
@@ -32,6 +42,9 @@ public class TicketingSystem {
         }
     }
     
+    /**
+     * CustomerAdd, adds a new customer to the queue.
+     */
     public static class CustomerAdd extends TimerTask { //inner class
        public int num = 1; // so that num will properly increment instead of being reinitialised each call
        
